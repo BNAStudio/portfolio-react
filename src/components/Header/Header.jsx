39b5ede0b6e8910f94825d93388a9b5afcba0106
Daniel Marcovich <danielmark999@gmail.com>
2022-09-30 19:46:22 -0500

    feat(core): adding .eslinttr rule
    adding non unescaped entities rule on .eslintrc config

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,14 @@
 // import PropTypes from "prop-types";
 
 const Header = () => {
-	return <div>Header</div>;
+	return (
+		<header>
+			<div className="container header__container">
+				<h5>Hello I'm</h5>
+				<h1>Daniel</h1>
+			</div>
+		</header>
+	);
 };
 
 Header.propTypes = {};

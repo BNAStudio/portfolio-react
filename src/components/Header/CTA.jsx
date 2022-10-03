61713e5d3099f83a62cb7efda5cdd0de3bab44eb
Daniel Marcovich <danielmark999@gmail.com>
2022-10-02 19:58:59 -0500

    feat(core|components): add react-icons
    adding react icons dependencies and create links for social media

--- src/components/Header/CTA.jsx
+++ src/components/Header/CTA.jsx
@@ -1,6 +1,5 @@
 // import PropTypes from 'prop-types'
 import CV from "../../assets/CV.pdf";
-import MySelf from "../../assets/myself.png";
 
 const CTA = () => {
 	return (
@@ -11,9 +10,6 @@ const CTA = () => {
 			<a href="#contact" className="btn btn-primary">
 				Let's talk
 			</a>
-			<div className="myself">
-				<img src={MySelf} alt="portfolio author image" />
-			</div>
 		</div>
 	);
 };
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,7 @@
 // import PropTypes from "prop-types";
+import HeaderSocial from "./HeaderSocial";
 import CTA from "./CTA";
+import MySelf from "../../assets/myself.png";
 
 const Header = () => {
 	return (
@@ -9,6 +11,13 @@ const Header = () => {
 				<h1>Daniel</h1>
 				<h5 className="text-light">FrontEnd Developer </h5>
 				<CTA />
+				<HeaderSocial />
+				<div className="myself">
+					<img src={MySelf} alt="portfolio author image" />
+				</div>
+				<a href="#contact" className="scroll-down">
+					Scroll down
+				</a>
 			</div>
 		</header>
 	);

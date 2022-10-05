56cbc171ad4f239ac99821389e4465a9ec57f8ef
Daniel Marcovich <danielmark999@gmail.com>
2022-10-04 22:34:08 -0500

    feat(components): header styles | HeaderCat component

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,9 +1,9 @@
+export { default as About } from "./About/About.jsx";
+export { default as Contact } from "./Contact/Contact.jsx";
+export { default as Experience } from "./Experience/Experience.jsx";
+export { default as Footer } from "./Footer/Footer.jsx";
 export { default as Header } from "./Header/Header.jsx";
 export { default as Nav } from "./Nav/Nav.jsx";
-export { default as About } from "./About/About.jsx";
-export { default as Experience } from "./Experience/Experience.jsx";
-export { default as Services } from "./Services/Services.jsx";
 export { default as Portfolio } from "./Portfolio/Portfolio.jsx";
+export { default as Services } from "./Services/Services.jsx";
 export { default as Testimonials } from "./Testimonials/Testimonials.jsx";
-export { default as Contact } from "./Contact/Contact.jsx";
-export { default as Footer } from "./Footer/Footer.jsx";

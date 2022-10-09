69b1b604b3b153ad431924709a3c8db431b1820f
Daniel Marcovich <danielmark999@gmail.com>
2022-10-09 03:02:18 -0500

    feat(components): experience | Services | components
    finish Experience component | create Services component | update index.css remove height section |
    update header.css remove padding in .myself class mediaquery mobile

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,9 +1,105 @@
 // import PropTypes from "prop-types";
+import { AiFillCheckCircle } from "react-icons/ai";
+import './Experience.css'
 
 const Experience = () => {
 	return (
 		<section id="experience">
-			Experience
+			<h5>¿What skills i have?</h5>
+			<h2>My experience</h2>
+
+			<div className="container experience__container">
+				<div className="experience__frontend">
+					<h3>Frontend development</h3>
+					<div className="experience__content">
+						<article className="experience__details">
+							<AiFillCheckCircle
+							className="experience__details-icon" />
+						<div>
+							<h4>HTML</h4>
+							<small clasName='text-light'>Experienced</small>
+						</div>
+						</article>
+
+						<article className="experience__details">
+							<AiFillCheckCircle className="experience__details-icon" />
+							<div>
+								<h4>CSS</h4>
+								<small clasName='text-light'>Intermediate</small>
+							</div>
+						</article>
+						
+						<article className="experience__details">
+							<AiFillCheckCircle className="experience__details-icon" />
+							<div>
+								<h4>Javascript</h4>
+								<small clasName='text-light'>Experienced</small>
+							</div>
+						</article>
+						
+						<article className="experience__details">
+							<AiFillCheckCircle className="experience__details-icon" />
+							<div>
+								<h4>Bootstrap</h4>
+								<small clasName='text-light'>Experienced</small>
+							</div>
+						</article>
+						
+						<article className="experience__details">
+							<AiFillCheckCircle className="experience__details-icon" />
+							<div>
+								<h4>React</h4>
+								<small clasName='text-light'>Intermediate</small>
+							</div>
+						</article>
+					</div>
+				</div>
+				{/* --- BACKEND --- */}
+				<div className="experience__backend">
+				<h3>Backend development</h3>
+					<div className="experience__content">
+						<article className="experience__details">
+							<AiFillCheckCircle className="experience__details-icon" />
+							<div>
+								<h4>Mongo DB</h4>
+								<small clasName='text-light'>Intermediated</small>
+							</div>
+						</article>
+
+						<article className="experience__details">
+							<AiFillCheckCircle className="experience__details-icon" />
+							<div>
+								<h4>PHP</h4>
+								<small clasName='text-light'>Intermediate</small>
+							</div>
+						</article>
+						
+						<article className="experience__details">
+							<AiFillCheckCircle className="experience__details-icon" />
+							<div>
+								<h4>MyPQL</h4>
+								<small clasName='text-light'>Experienced</small>
+							</div>
+						</article>
+						
+						<article className="experience__details">
+							<AiFillCheckCircle className="experience__details-icon" />
+							<div>
+								<h4>Python</h4>
+								<small clasName='text-light'>Experienced</small>
+							</div>
+						</article>
+						
+						<article className="experience__details">
+							<AiFillCheckCircle className="experience__details-icon" />
+							<div>
+								<h4>Node</h4>
+								<small clasName='text-light'>Intermediate</small>
+							</div>
+						</article>
+					</div>
+				</div>
+			</div>
 		</section>
 	);
 };

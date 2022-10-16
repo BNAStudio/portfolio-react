93e7511650ff0d5525c719085335d8704c6d8468
Daniel Marcovich <danielmark999@gmail.com>
2022-10-16 01:46:24 -0500

    feat(components): adding Footer component
    create Footer component | adding Footer component styles

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,33 @@
 // import PropTypes from "prop-types";
+import './Footer.css'
+import imageProject from "../../assets/myself.png";
+import { AiOutlineFacebook, AiFillGithub, AiFillLinkedin } from 'react-icons/ai'
 
 const Footer = () => {
-	return <div>Footer</div>;
+	return (
+		<footer>
+			<a className="footer__logo" href={imageProject}></a>
+			<ul className="permalinks">
+				<li><a href="#">Home</a></li>
+				<li><a href="#about">About</a></li>
+				<li><a href="#experience">Experience</a></li>
+				<li><a href="#services">Services</a></li>
+				<li><a href="#portfolio">Portfolio</a></li>
+				<li><a href="#testimonials">Testimonials</a></li>
+				<li><a href="#contact">Contact</a></li>
+			</ul>
+
+			<div className="footer__socials">
+				<a href="https://facebook.com"><AiOutlineFacebook/></a>
+				<a href="https://github.com"><AiFillGithub /></a>
+				<a href="https://linkedin.com"><AiFillLinkedin /></a>
+			</div>
+
+			<div className="footer__copyright">
+				<small>&copy; BNA Studio |  All rights reserved </small>
+			</div>
+		</footer>
+	);
 };
 
 Footer.propTypes = {};
